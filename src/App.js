1f5ce41c222c4cc5621439d8449dfba233ade12f
Jeff Trojan <j.dauda@alustudent.com>
2023-02-13 17:21:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          TedX ALU, &copy; 2022 <code>What if?</code> 
+          <br />
+          Coming soon...
+        </p>
+        
+          
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
