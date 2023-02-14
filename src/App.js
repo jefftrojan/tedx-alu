a036508ba4af755797d24e1b2c903568fdd7978e
Jeff Trojan <j.dauda@alustudent.com>
2023-02-14 23:01:30 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="header">
         
         <p>
           TedX ALU, &copy; 2022 <code>What if?</code> 
@@ -14,6 +14,21 @@ function App() {
           
       
       </header>
+
+      <div className='footer'>
+      <p className='left-foot'>
+
+      TEDxALURwanda is founded in the spirit of TED’s mission of Ideas Worth Spreading. <br/>The student-led organisation’s goal is to create unique experiences composed of interdisciplinary talks and engaging exchanges for all attendees..
+This independent TEDx Organisation is operated under license from TED.
+      </p>
+
+      </div>
+
+      <p className='right-foot'>
+
+      The student-led organisation’s goal is to create unique experiences composed of interdisciplinary talks and engaging exchanges for all attendees..
+      This independent TEDx Organisation is operated under license from TED.<br/>
+      </p>
     </div>
   );
 }
